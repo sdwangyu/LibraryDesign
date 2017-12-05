0fee0dd4fd6c07e31a5afb50c114b00189aec6f5
Dongmantc <1527728702@qq.com>
2017-12-05 20:09:13 +0800

    界面大体上是这样了

--- librarysystem.cpp
+++ librarysystem.cpp
@@ -6,9 +6,15 @@ LibrarySystem::LibrarySystem(QWidget *parent) :
     ui(new Ui::LibrarySystem)
 {
     ui->setupUi(this);
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
 LibrarySystem::~LibrarySystem()
 {
     delete ui;
+}
+
+void LibrarySystem::on_userwindowinformation_clicked()
+{
+    ui->stackedWidget_2->setCurrentIndex(0);
 }
