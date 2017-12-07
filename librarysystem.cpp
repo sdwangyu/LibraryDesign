036cef2d980cdea2583326c59b8510a9d4c24258
Dongmantc <1527728702@qq.com>
2017-12-07 14:50:42 +0800

    更新

--- librarysystem.cpp
+++ librarysystem.cpp
@@ -2337,7 +2337,7 @@ void LibrarySystem::on_userLogin_clicked()
                 //ui->useraccount->setFocus();
                 //ui->userpassword->clear();
                 //隐藏登录对话框
-                ui->mainwidget->setCurrentIndex(5);;//显示用户主窗口
+                ui->mainwidget->setCurrentIndex(4);;//显示用户主窗口
             }
             else {
                 QMessageBox::warning(this,tr("密码错误"),tr("请输入正确的密码."),QMessageBox::Ok);
@@ -2348,7 +2348,20 @@ void LibrarySystem::on_userLogin_clicked()
             //对用户账号和密码的检查，
         }
         else if(ui->loginforadmin->isChecked()){
-            ui->mainwidget->setCurrentIndex(5);;//显示管理员主窗口
+            if(signInAdmin(username, password)==1){
+                //ui->useraccount->clear();
+                //ui->useraccount->setFocus();
+                //ui->userpassword->clear();
+                //隐藏登录对话框
+                ui->mainwidget->setCurrentIndex(5);;//显示用户主窗口
+            }
+            else {
+                QMessageBox::warning(this,tr("密码错误"),tr("请输入正确的密码."),QMessageBox::Ok);
+                ui->useraccount->clear();
+                ui->useraccount->setFocus();
+                ui->userpassword->clear();
+            }
+            //对用户账号和密码的检查，
         }
 
 
